18f6c136f8bba57f6b245491120ee9590320fc93
josephsmith0705 <josephsmith0705@gmail.com>
2021-07-08 20:38:02 +0100

    Added heading subtitle

--- scripts.js
+++ scripts.js
@@ -20,17 +20,21 @@ const dropdown = {
 
 function addMouseOver(elements){
 	for (i=0; i<elements.length;i++){
-		elements[i].addEventListener('mouseover', e=>{
-			e.target.style.animation = 'hover 0.3s linear';
-			e.target.style.color = 'var(--dark-accent)';
-		})
-		elements[i].addEventListener('mouseout', e=>{
-			e.target.style.animation = 'unhover 0.15s linear';
-			setTimeout(() => {e.target.style.color = 'var(--main-white)';}, 140);
-		})
-		elements[i].addEventListener('click', e=>{
-			e.target.style.transform = scale(0.2);
-		})
+		if (elements[i].classList.contains('current-page')){
+			elements[i].style.pointerEvents = 'none'; //Deactivate link for current page
+		} else if(!elements[i].classList.contains('subtitle-trigger')){
+			elements[i].addEventListener('mouseover', e=>{
+				e.target.style.animation = 'hover 0.3s linear';
+				e.target.style.color = 'var(--dark-accent)';
+			})
+			elements[i].addEventListener('mouseout', e=>{
+				e.target.style.animation = 'unhover 0.15s linear';
+				setTimeout(() => {e.target.style.color = 'var(--main-white)';}, 140);
+			})
+			/*elements[i].addEventListener('click', e=>{
+				e.target.style.transform = scale(0.2);
+			})*/
+		}
 	}
 }
 
@@ -46,6 +50,28 @@ window.addEventListener('resize', e=>{
 	if (!dropdown.hidden && window.innerWidth > 780){
 		dropdown.hide();
 	}
+});
+
+/* Could be used to automatically hide/show subtitles regardless of quantity
+const subtitleTrigger = document.querySelectorAll('.subtitle-trigger');
+const navlinkSubtitle = document.querySelectorAll('.navlink-subtitle');
+for (i=0; i<subtitleTrigger.length; i++){
+	subtitleTrigger[i].addEventListener('mouseover', e=>{
+		e.target.style.opacity = '1';
+		e.target.style.animation = 'fadein 0.3s linear';
+	})
+	subtitleTrigger[i].addEventListener('mouseout', e=>{
+		setTimeout(() => {e.target.style.animation = 'fadein 0.3s reverse';}, 290);
+	})
+}*/
+
+const subtitleTrigger = document.querySelectorAll('.subtitle-trigger');
+const navlinkSubtitle = document.querySelectorAll('.navlink-subtitle');
+subtitleTrigger[1].addEventListener('mouseover', e=>{
+	navlinkSubtitle[0].style.opacity = '1';
+});
+subtitleTrigger[1].addEventListener('mouseout', e=>{
+	navlinkSubtitle[0].style.opacity = '0';
 });
 
 addMouseOver(document.querySelectorAll('.navlink'));
